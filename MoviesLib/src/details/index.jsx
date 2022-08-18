988d8e4f89d6e9599603ae52603b9e319ebbfd22
Caio <caioersb@gmail.com>
2022-08-18 16:43:27 -0300

    Update react, style

--- MoviesLib/src/details/index.jsx
+++ MoviesLib/src/details/index.jsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { Div } from "./style-details"
 
 function Details() {
     const { id } = useParams()
 
+    const [serie, setSerie] = useState({})
+
+    useEffect(() =>{
+        fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=291da6e68320eb0d68736c1c02a163fe&language=en-US`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+            const { name, overview, number_of_seasons, number_of_episodes, poster_path, tagline} = data
+
+            const serie = {
+                id,
+                name,
+                img:`https://image.tmdb.org/t/p/w500${poster_path}`,
+                overview,
+                number_of_seasons,
+                number_of_episodes,
+                tagline
+            }
+
+            setSerie(serie)
+
+        })
+    }, [id])
+
     return (
-        <h1>Página de detalhes</h1>
+        <Div>
+            <div className="serie">
+            <img src={serie.img} alt={serie.name} />
+            </div>
+
+            <div className="details">
+                <h1>{serie.name}</h1>
+                <span>{serie.tagline}</span>
+                <span>{serie.overview}</span>
+                <span>{serie.number_of_seasons} seasons</span>
+                <span>{serie.number_of_episodes} episodes</span>
+                <button>Go back</button>
+            </div>
+            
+        </Div>
     )
 }
 
---
+++ MoviesLib/src/details/style-details.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const Div = styled.div`
+    background: black;
+    color: white;
+
+
+    img{
+        width: 300px;
+        border-radius: 8px;
+    }
+`
