3b505d57865f2b00605e1efa251a24b3152a8e0f
Caio Eduardo Ribeiro Bento <caioersb@gmail.com>
2024-10-22 13:52:17 -0300

    juntando séries e filmes

--- TMDB-API/src/App.jsx
+++ TMDB-API/src/App.jsx
@@ -7,14 +7,20 @@ import './styles/App.css'
 function App() {
 
   const [movies, setMovies] = useState([])
+  const [series, setSeries] = useState([])
   const img_path = 'https://image.tmdb.org/t/p/w500'
 
   useEffect(() =>{
-  // consumindo a api
+  // consumindo a api para filmes
 
-  fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`)
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
   .then(response => response.json())
   .then(data => setMovies(data.results))
+
+  // consumindo a api para séries
+  fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    .then(response => response.json())
+    .then(data => setSeries(data.results))
   }, [])
 
   return (
@@ -22,8 +28,9 @@ function App() {
       <h1>TMDB</h1>
       <hr />
 
+      <h2>Filmes Populares</h2>
       <SeriesList>
-
+      
       {movies.map(movie =>{
           return(
             <Series key={movie.id}>
@@ -35,6 +42,19 @@ function App() {
           )
         })}
 
+      </SeriesList>
+
+      <h2>Séries Populares</h2>
+      <SeriesList>
+        {series.map(tvShow => (
+          <Series key={tvShow.id}>
+            <Link to={`/details/${tvShow.id}`}>
+              <img src={`${img_path}${tvShow.poster_path}`} alt={tvShow.name} />
+            </Link>
+            <span>{tvShow.name}</span>
+            <span>{tvShow.vote_average.toFixed(1)}</span>
+          </Series>
+        ))}
       </SeriesList>
 
     </Container>
